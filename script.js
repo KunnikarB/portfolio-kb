5e4bd8a0abf9fbe04d71a1631130e5894d59f688
kunnikarB <kunnikarboon@gmail.com>
2026-03-19 10:46:34 +0700

    Updated Script and modified footer

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const toggle = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
 
-toggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+if (toggle && navLinks) {
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
+
+const links = document.querySelectorAll('#nav-links a');
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+  });
 });
