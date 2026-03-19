b1f3427c02fbe554e2c62f5b46173887234b57a3
kunnikarB <kunnikarboon@gmail.com>
2026-03-19 19:23:40 +0700

    Update contact form using EmailJs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hamburger menu toggle
 const toggle = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
 
@@ -13,4 +14,35 @@ links.forEach((link) => {
   link.addEventListener('click', () => {
     navLinks.classList.remove('active');
   });
+});
+
+
+// Contact form handling with EmailJS
+const form = document.getElementById('contact-form');
+const status = document.getElementById('form-status');
+const button = document.getElementById('submit-btn');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  button.innerText = 'Sending...';
+  button.disabled = true;
+
+  emailjs
+    .sendForm(
+      'service_xzcywie', // from EmailJS
+      'template_0e6u8sf', // from EmailJS
+      this,
+    )
+    .then(() => {
+      status.innerText = '✅ Message sent successfully!';
+      form.reset();
+    })
+    .catch(() => {
+      status.innerText = '❌ Failed to send message. Try again.';
+    })
+    .finally(() => {
+      button.innerText = 'Send Message';
+      button.disabled = false;
+    });
 });
